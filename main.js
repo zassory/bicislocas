e637b54a1f09a2a2c56796e36432fc8c36ab3425
Matias <mbensan@gmail.com>
2024-02-21 11:22:33 -0300

    Se valida el largo del nombre del formulario

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function validateForm () {
   // your code here
+  // 1. Traerme los valores del formulario
+  const name = $("#name").val();
+
+  // 2. Validar el largo del nombre
+  if (name.length < 3) {
+    alert("El nombre es muy corto");
+  }
+  else if (name.length > 8) {
+    alert("El nombre no puede ser mayor a 8");
+  }
 }
